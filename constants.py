2cf04e52c769363740459590521deb5fd662220d
ProtsenkoM <mishunya4@gmail.com>
2023-11-26 17:30:42 +0200

    update constants

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-HUB_URL = "http://localhost:4444/wd/hub"
+HUB_URL = "http://192.168.0.106:4444/wd/hub"
 #HUB_URL = "http://test:test-123@localhost:4444/wd/hub"
 
 
